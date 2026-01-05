e97a1293f78e729ab9bb3c4cc33eca948b7d1032
KinjalKathrecha <kinjalkathrecha@gmail.com>
2026-01-05 16:57:53 +0530

    templates updated

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from leads.views import home_page
+from leads.views import lead_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
