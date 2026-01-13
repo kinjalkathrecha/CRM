b594129bffa6c2631a47985c0ab22d2163ebe354
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-01-13 16:26:27 +0530

    set database on render

--- CRM/settings.py
+++ CRM/settings.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 import dj_database_url
 
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
 
 
 load_dotenv()
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,19 +86,19 @@ WSGI_APPLICATION = 'CRM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env("DB_NAME"),
-#         'USER':env("DB_USER"),
-#         'PASSWORD':env("DB_PASSWORD"),
-#         'HOST':env("DB_HOST"),
-#         'PORT':env("DB_PORT"),
-#         'OPTIONS': {
-#             'options': '-c search_path=public,django,static' # Tells Django to look in 'public'
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER':env("DB_USER"),
+        'PASSWORD':env("DB_PASSWORD"),
+        'HOST':env("DB_HOST"),
+        'PORT':env("DB_PORT"),
+        'OPTIONS': {
+            'options': '-c search_path=public,django,static' # Tells Django to look in 'public'
+        },
+    }
+}
 #postgres-password-'kinjal123'
 
 # Password validation
