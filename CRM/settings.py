bc054ffd16e51691cb1b866721bbf8c4d423198a
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-01-15 11:37:21 +0530

    fix done

--- CRM/settings.py
+++ CRM/settings.py
@@ -170,3 +170,17 @@ if not DEBUG:
     X_FRAME_OPTIONS = "DENY"
 
     ALLOWED_HOSTS = ["*"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
