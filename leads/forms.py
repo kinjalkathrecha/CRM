241756058be7b4e4e7e9e6f877e6a66143eeb8d8
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-01-20 11:20:59 +0530

    file forms

--- leads/forms.py
+++ leads/forms.py
@@ -17,6 +17,7 @@ class LeadModelForm(forms.ModelForm):
             'description',
             'phone_number',
             'email',
+            'profile_picture',
         )
 
     def clean_first_name(self):
